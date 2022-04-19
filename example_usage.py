b5a366cab840c9710ab423ff3a414cd3a40d4100
JacksonBurns <JacksonBurns@users.noreply.github.com>
2022-04-19 14:58:49 -0400

    working example

--- example_usage.py
+++ example_usage.py
@@ -1,2 +1,55 @@
+import numpy as np
+
+from extended_train_test_split.interfaces import (
+    train_test_split_molecules,
+    train_test_split_images,
+)
+
 from extended_train_test_split import train_test_split
-from extended_train_test_split.interfaces import train_test_split_molecules
+
+X_train, X_test, y_train, y_test = train_test_split(
+    np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]]),
+    np.array([10, 11, 12]),
+    test_size=0.2,
+    train_size=0.8,
+    shuffle=True,
+    splitter='random',
+    random_state=42,
+)
+
+print(X_train, X_test, y_train, y_test)
+
+X_train, X_test, y_train, y_test = train_test_split_molecules(
+    ['C', 'CC', 'CCC', 'CCCC', 'CCCCC'],
+    np.array([10, 11, 12, 13, 14]),
+    test_size=0.2,
+    train_size=0.8,
+    shuffle=True,
+    return_as='fprint',
+    splitter='random',
+    hopts={'random_state': 42},
+    fprints_hopts={'radius': 2},
+)
+
+print(X_train, X_test, y_train, y_test)
+
+
+X_train, X_test, y_train, y_test = train_test_split_molecules(
+    ['C', 'CC', 'CCC', 'CCCC', 'CCCCC'],
+    np.array([10, 11, 12, 13, 14]),
+    test_size=0.2,
+    train_size=0.8,
+    return_as='fprint',
+    splitter='kennard_stone',
+    fprints_hopts={'radius': 2},
+)
+
+print(X_train, X_test, y_train, y_test)
+'''
+Each of these specialized interfaces needs to take additional keyword args
+specific to their use case. For example, molecules needs to take a
+fingerprint (and maybe another **fprint_hopts)
+
+All other special hyperparamters for the splitters need to be taken
+by the interfaces and then just passed onto train_test_split.
+'''
--- extended_train_test_split/interfaces/__init__.py
+++ extended_train_test_split/interfaces/__init__.py
@@ -1,8 +1,2 @@
-# we want imports to work like this
-# include this is this init so the init in the parent can import this
-# as well, making it easily accesible as
-# from simsplitchem import splitchem
-# and
-# from simsplitchem.samplers import random
-
-# from kennard_stone import kennardstone
+from .train_test_split_molecules import train_test_split_molecules
+from .train_test_split_images import train_test_split_images
